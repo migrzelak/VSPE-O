0554cdaad5b9813b9cee5210b82b17ab1d968bba
Michał Grzelak <michal.grzelak@intel.com>
2026-05-07 00:07:20 +0200

    drm/i915/buf_trans: enumerate LT's VS/PE-O indices
    Encapsulate LT's VS/PE-O's indices in enum lt_vspeo_index.
    
    Treat error case as referring to index -1. Refer to it as LT_ERR.
    
    Tables 1-4 are not used at all and are most likely to be zeroed. Refer
    to them as _LT_unusedN.
    
    5th table is used for any mode below DP 2.0 (exclusive). Refer to it as
    LT_DP14.
    
    6th table is used for any mode above DP 2.0 (inclusive). Refer to it
    as LT_DP21.
    
    Indices for other tables have not yet been observed to be used as of
    now.
    
    Signed-off-by: Michał Grzelak <michal.grzelak@intel.com>

--- drivers/gpu/drm/i915/display/intel_ddi_buf_trans.c
+++ drivers/gpu/drm/i915/display/intel_ddi_buf_trans.c
@@ -1786,21 +1786,21 @@ xe3plpd_get_lt_buf_trans(struct intel_encoder *encoder,
 		return intel_get_buf_trans(&xe3plpd_lt_trans_dp14, n_entries);
 }
 
-static int
+static enum lt_vspeo_index
 lt_compute_index(const struct intel_crtc_state *crtc_state)
 {
 	if (intel_crtc_has_dp_encoder(crtc_state)) {
 		if (intel_dp_is_uhbr(crtc_state))
-			return 5;
+			return LT_DP21;
 		else
-			return 4;
+			return LT_DP14;
 	}
 
 	drm_WARN(to_intel_display(crtc_state)->drm, 1,
 		 "non-DP (%d) encoder asks to compute VS/PE-O index\n",
 		 crtc_state->output_types);
 
-	return -EINVAL;
+	return LT_ERR;
 }
 
 static int
--- drivers/gpu/drm/i915/display/intel_ddi_buf_trans.h
+++ drivers/gpu/drm/i915/display/intel_ddi_buf_trans.h
@@ -74,6 +74,18 @@ struct intel_ddi_buf_trans {
 	u8 hdmi_default_entry;
 };
 
+enum lt_vspeo_index {
+	LT_ERR = -1,
+
+	_LT_unused0 = 0,
+	_LT_unused1,
+	_LT_unused2,
+	_LT_unused3,
+
+	LT_DP14 = 4,
+	LT_DP21
+};
+
 bool is_hobl_buf_trans(const struct intel_ddi_buf_trans *table);
 
 void intel_ddi_buf_trans_init(struct intel_encoder *encoder);
