f7e5f88113391264c0bb7565782f8d3f31c60630
Michał Grzelak <michal.grzelak@intel.com>
2026-05-29 00:22:02 +0200

    drm/i915: override Snps's VS/PE when requested
    Add accessor function for Snps to read requested table from VBT #57.
    Parse the requested table and transform data into port's buffer.
    
    For C20, use 6th table if encoder supports DP 2.0 or higher. Otherwise
    use 5th table for DP.
    
    For C20, tables 1-4 are not used at all and are most likely to be
    zeroed. 5th table is used for any mode below DP 2.0 (exclusive). 6th
    table is used for any mode above DP 2.0 (inclusive).
    
    For C10, use 2nd table for external DP if encoder supports any mode
    beyond or including HBR2. Use 1st table if external DP encoder supports
    anything lower than HBR2. For eDP, use 4th table if encoder supports
    HBR3. Otherwise use 3rd table for eDP.
    
    For C10, 1st table is used for external DP with modes below HBR2
    (exclusive). 1st table is also used as a fallback for non-DPs. 2nd
    table is used for external DP with modes higher than HBR2 (inclusive).
    3rd table is used for eDP with modes lower than HBR3 (exclusive). 4th
    table is used for eDP with modes higher than HBR3 (inclusive).
    
    Indices for other tables have not yet been observed to be used as of
    now.
    
    There are no changes to intel_ddi_dp_level() since selection of correct
    row of intel_ddi_buf_trans_entry is same as when no override request has
    been done.
    
    v4->v5
    - blend index computation with table parsing
    - remove enums entirely
    - change funcs prefix from snps_ to mtl_ (Suraj)
    - add spaces around operators (Suraj)
    - remove spaces after type casting (Suraj)
    - remove INTEL_DISPLAY_STATE_WARN (Suraj)
    
    v3->v4
    - stick to solely changing VBT data into current structures (Jani)
    - move iterator declaration to declaration block (Suraj)
    
    v2->v3
    - remove unnecessary braces from if block (Suraj)
    - return -EINVAL instead of -1 (Suraj)
    
    Signed-off-by: Michał Grzelak <michal.grzelak@intel.com>

--- drivers/gpu/drm/i915/display/intel_bios.h
+++ drivers/gpu/drm/i915/display/intel_bios.h
@@ -76,6 +76,9 @@ intel_bios_encoder_data_lookup(struct intel_display *display, enum port port);
 const struct intel_ddi_buf_trans *
 intel_bios_get_lt_vspeo(const struct intel_bios_encoder_data *devdata,
 			int idx);
+const struct intel_ddi_buf_trans *
+intel_bios_get_cx0_vspeo(const struct intel_bios_encoder_data *devdata,
+			 int idx);
 
 bool intel_bios_encoder_requests_vspeo(const struct intel_bios_encoder_data *devdata);
 bool intel_bios_encoder_supports_dvi(const struct intel_bios_encoder_data *devdata);
